1aaf1b6d47bc8e418cb9db840cae276e920b2e46
SerafimE <38590113+SerafimE@users.noreply.github.com>
2018-07-07 16:55:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-$file = file_get_contents("base.json");
-$decode = json_decode($file, true);
+$jsonString = file_get_contents(__DIR__ . '/base.json');
+$data = json_decode($jsonString, true);
 ?>
-
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
     <title>Домашнее задание к лекции 2.1</title>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
@@ -22,20 +22,24 @@ $decode = json_decode($file, true);
             <tr>
                 <th>Имя</th>
                 <th>Фамилия</th>
-                <th>Адрес</th>
-                <th>Телефон</th>
+                <th>Индекс</th>
+                <th>Город</th>
+                <th>Улица</th>
+                <th>Дом</th>
+                <th>Квартира</th>
+                <th>Телефон мобильный</th>
+                <th>Телефон "другой"</th>
             </tr>
             </thead>
             <tbody>
-            <?php foreach ($decode as $phonebook):?>
+            <?php foreach ($data as $phone_book): ?>
                 <tr>
-                    <td><?=$phonebook['firstName']; ?></td>
-                    <td><?=$phonebook['lastName']; ?></td>
-                    <td><?=$phonebook['address']; ?></td>
-                    <td><?=$phonebook['phoneNumber']; ?></td>
+                    <td><?php echo $phone_book['firstName']; ?></td>
+                    <td><?php echo $phone_book['lastName']; ?></td>
+                    <?php foreach ($phone_book['address'] as $address => $val) echo "<td>$val </td>" ?>
+                    <?php foreach ($phone_book['phoneNumber'] as $phoneNumber => $val_num) echo "<td>$val_num </td>" ?>
                 </tr>
-            <?php endforeach;?>
-
+            <?php endforeach; ?>
             </tbody>
         </table>
     </div>
